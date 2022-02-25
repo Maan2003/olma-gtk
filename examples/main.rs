9369256b1a11e2c60e3445d6d3dbaa02899f212e
Maan2003 <manmeetmann2003@gmail.com>
2022-02-25 17:34:05 +0530

    Button!

--- examples/main.rs
+++ examples/main.rs
@@ -1,19 +1,25 @@
 #![feature(generic_associated_types, type_alias_impl_trait)]
-use olma_gtk::views::Label;
+use olma_gtk::views::{Button, Label};
 use olma_gtk::{AnyView, App, View};
 
-struct Model;
+struct Model {
+    num: i32,
+}
+
+enum Msg {}
 impl App for Model {
     type Msg = ();
     type View<'a> = impl View<'a>;
 
-    fn update(&mut self, msg: Self::Msg) {}
+    fn update(&mut self, msg: Self::Msg) {
+        self.num += 1;
+    }
 
     fn view(&self) -> Self::View<'_> {
-        Label("Hello world!")
+        Button::new(format!("count: {}", self.num)).on_click(|| {})
     }
 }
 
 fn main() {
-    olma_gtk::launch(Model);
+    olma_gtk::launch(Model { num: 0 });
 }
--- src/views.rs
+++ src/views.rs
@@ -13,9 +13,62 @@ mod label {
         }
 
         fn update(self, widget: &gtk::Widget) {
-            todo!()
+            let label = widget.downcast_ref::<gtk::Label>().unwrap();
+            label.set_label(self.0);
         }
     }
 }
 
+mod button {
+    use std::borrow::Cow;
+
+    use crate::{app::Messenger, view::View};
+    use gtk::prelude::*;
+
+    // TODO: allow updating the on_click
+    pub struct Button<'a> {
+        label: Cow<'a, str>,
+        on_click: Box<dyn Fn()>,
+    }
+
+    impl<'a> Button<'a> {
+        pub fn new(label: impl Into<Cow<'a, str>>) -> Self {
+            Self {
+                label: label.into(),
+                on_click: Box::new(|| {}),
+            }
+        }
+
+        pub fn on_click<F: 'static, M: 'static>(mut self, on_click: F) -> Self
+        where
+            F: Fn() -> M,
+        {
+            let msgr = Messenger::current().unwrap();
+            self.on_click = Box::new(move || {
+                msgr.send(Box::new(on_click()));
+            });
+            self
+        }
+    }
+
+    impl<'a> View<'a> for Button<'a> {
+        fn build(self) -> gtk::Widget {
+            let btn = gtk::Button::builder().label(&self.label).build();
+            let _ = btn.connect_clicked(move |_| {
+                (self.on_click)();
+            });
+
+            btn.upcast::<gtk::Widget>()
+        }
+
+        fn update(self, widget: &gtk::Widget) {
+            widget
+                .downcast_ref::<gtk::Button>()
+                .unwrap()
+                .set_label(&self.label);
+        }
+    }
+}
+
+pub use button::Button;
 pub use label::Label;
